baae686243e92c1451553b1e3f67fba27a92ab8b
unknown <manubazlo@gmail.com>
2024-03-05 12:02:28 +0100

    añadidos comentarios al codigo y añadidos los archivos de javadoc

--- src/cuentas/CCuenta.java
+++ src/cuentas/CCuenta.java
@@ -1,8 +1,82 @@
 package cuentas;
 
+
+/**
+ * Clase CCuenta, representa una cuenta bancaria y aloja los datos relativos a ella,
+ * como el titular, numero de cuenta, saldo e intereses
+ * 
+ * @author bofh
+ */
 public class CCuenta {
 
+    private String nombre;
+    private String cuenta;
+    private double saldo;
+    private double tipoInterés;
+
     /**
+     * Constructor por defecto
+     */
+    public CCuenta()
+    {
+    }
+
+    /**
+     * Constructor que inicializa todas las propiedades de la cuenta
+     * @param nom nombre completo del titular
+     * @param cue numero de cuenta
+     * @param sal saldo inicial 
+     * @param tipo tipo de interés
+     */
+    public CCuenta(String nom, String cue, double sal, double tipo)
+    {
+        nombre =nom;
+        cuenta=cue;
+        saldo=sal;
+    }
+
+    /**
+     * Obtiene el saldo actual de la cuenta
+     * @return saldo actual
+     */
+    public double estado()
+    {
+        return getSaldo();
+    }
+
+    /**
+     * Ingresar una cantidad de dinero en cuenta
+     * 
+     * @param cantidad dinero a ingresar
+     * @throws Exception 
+     */
+    public void ingresar(double cantidad) throws Exception
+    {
+        if (cantidad<0)
+            throw new Exception("No se puede ingresar una cantidad negativa");
+        setSaldo(getSaldo() + cantidad);
+    }
+
+    /**
+     * Retirar dinero de la cuenta, comprueba si hay suficiente dinero en cuenta
+     * antes de retirar el dinero, de no haberlo devuelve una excepcion
+     * 
+     * @param cantidad Cantidad a retirar
+     * @throws Exception 
+     */
+    public void retirar(double cantidad) throws Exception
+    {
+        if (cantidad <= 0)
+            throw new Exception ("No se puede retirar una cantidad negativa");
+        if (estado()< cantidad)
+            throw new Exception ("No se hay suficiente saldo");
+        setSaldo(getSaldo() - cantidad);
+    }
+    
+    
+    //Getters y setters generados automáticamente
+    
+     /**
      * @return the nombre
      */
     public String getNombre() {
@@ -56,43 +130,5 @@ public class CCuenta {
      */
     public void setTipoInterés(double tipoInterés) {
         this.tipoInterés = tipoInterés;
-    }
-
-
-    private String nombre;
-    private String cuenta;
-    private double saldo;
-    private double tipoInterés;
-
-    public CCuenta()
-    {
-    }
-
-    public CCuenta(String nom, String cue, double sal, double tipo)
-    {
-        nombre =nom;
-        cuenta=cue;
-        saldo=sal;
-    }
-
-    public double estado()
-    {
-        return getSaldo();
-    }
-
-    public void ingresar(double cantidad) throws Exception
-    {
-        if (cantidad<0)
-            throw new Exception("No se puede ingresar una cantidad negativa");
-        setSaldo(getSaldo() + cantidad);
-    }
-
-    public void retirar(double cantidad) throws Exception
-    {
-        if (cantidad <= 0)
-            throw new Exception ("No se puede retirar una cantidad negativa");
-        if (estado()< cantidad)
-            throw new Exception ("No se hay suficiente saldo");
-        setSaldo(getSaldo() - cantidad);
     }
 }
--- src/cuentas/Main.java
+++ src/cuentas/Main.java
@@ -1,7 +1,20 @@
 package cuentas;
 
+/**
+ * Clase principal, simulador de operaciones sobre cuenta bancaria
+ * Crea una cuenta de la clase CCuenta, muestra su saldo y realiza
+ * alguna operación sobre ella, mostrando información por pantalla
+ * 
+ * @author Manuel Bazarra
+ */
 public class Main {
 
+    /**
+     * Método principal, contienen la logica principal del programa, instancia una cuenta
+     * y opera sobre ella
+     * 
+     * @param args 
+     */
     public static void main(String[] args) {
         CCuenta cuenta1;
         double saldoActual;
@@ -13,6 +26,13 @@ public class Main {
         operativa_cuenta(cuenta1, 0f); //Enviamos 0 mientras no se especifica que hacer con el parametro
     }
 
+    
+    /**
+     * Método que llama a las operaciones de retirada e ingreso en cuenta
+     * 
+     * @param cuenta1 objeto sobre el que se quiere operar
+     * @param cantidad cantidad para operar, de momento sin uso
+     */
     private static void operativa_cuenta(CCuenta cuenta1, float cantidad) {
         try {
             cuenta1.retirar(2300);
